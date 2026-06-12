f40f9bc8c2872a8ace1ebd7a68a97259a6921777
Yousef Khani <cfyakan@gmail.com>
2026-06-12 19:37:35 +0330

    Update rss_fetcher.go

--- internal/fetcher/rss_fetcher.go
+++ internal/fetcher/rss_fetcher.go
@@ -3,6 +3,7 @@ package fetcher
 import (
 	"io"
 	"net/http"
+	"time"
 )
 
 type RSSFetcher struct {
@@ -10,7 +11,19 @@ type RSSFetcher struct {
 }
 
 func (r *RSSFetcher) Fetch() ([]byte, error) {
-	resp, err := http.Get(r.URL)
+
+	client := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+
+	req, err := http.NewRequest("GET", r.URL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (writeup-fetcher)")
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
