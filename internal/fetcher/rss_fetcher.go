a25898b2bb3b578c13537e294f405785c37ff655
Yousef Khani <cfyakan@gmail.com>
2026-06-12 18:52:47 +0330

    Update rss_fetcher.go

--- internal/fetcher/rss_fetcher.go
+++ internal/fetcher/rss_fetcher.go
@@ -1,1 +1,20 @@
-1
+package fetcher
+
+import (
+	"io"
+	"net/http"
+)
+
+type RSSFetcher struct {
+	URL string
+}
+
+func (r *RSSFetcher) Fetch() ([]byte, error) {
+	resp, err := http.Get(r.URL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
+}
