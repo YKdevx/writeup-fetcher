2b14a3adea98bc93bf6f6cb172bf2baf8eff0275
Yousef Khani <cfyakan@gmail.com>
2026-06-12 19:02:01 +0330

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,29 @@
-1
+package main
+
+import (
+	"fmt"
+
+	"writeup-fetcher/internal/fetcher"
+	"writeup-fetcher/internal/parser"
+)
+
+func main() {
+
+	rssFetcher := fetcher.RSSFetcher{
+		URL: "https://medium.com/feed/tag/cybersecurity",
+	}
+
+	data, err := rssFetcher.Fetch()
+	if err != nil {
+		panic(err)
+	}
+
+	rss, err := parser.ParseRSS(data)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, item := range rss.Channel.Items {
+		fmt.Println(item.Title)
+	}
+}
