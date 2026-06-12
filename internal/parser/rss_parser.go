c05d4ba7ab2d5bbdb52f11f1ed422c15e48c2629
Yousef Khani <cfyakan@gmail.com>
2026-06-12 18:55:22 +0330

    Update rss_parser.go

--- internal/parser/rss_parser.go
+++ internal/parser/rss_parser.go
@@ -1,1 +1,17 @@
-1
+package parser
+
+import "encoding/xml"
+
+type RSS struct {
+	Channel Channel `xml:"channel"`
+}
+
+type Channel struct {
+	Items []RSSItem `xml:"item"`
+}
+
+type RSSItem struct {
+	Title   string `xml:"title"`
+	Link    string `xml:"link"`
+	PubDate string `xml:"pubDate"`
+}
